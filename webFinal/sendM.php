af5435348416e9b091db6e877c3360b076eaa0c1
RMiranda <64057933+rmirandamix@users.noreply.github.com>
2020-11-06 13:14:45 -0300

    tercero

--- webFinal/sendM.php
+++ webFinal/sendM.php
@@ -1,1 +1,23 @@
-sendM.php
+<?php
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$fono = $_POST['fono'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $correo . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Nombre Contacto: " . $nombre . ",\r\n";
+$mensaje .= "E-Mail Contacto: " . $correo . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'rorro.mix@gmail.com';
+$asunto = 'Contacto Web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
