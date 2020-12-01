cfa627aedf902c6eb6c9441f481b63abf8bf7a1c
RMiranda <64057933+rmirandamix@users.noreply.github.com>
2020-12-01 16:14:19 -0300

    enviar

--- webFinal/sendM.php
+++ webFinal/sendM.php
@@ -1,23 +1,33 @@
 <?php
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$fono = $_POST['fono'];
-$mensaje = $_POST['mensaje'];
 
-$header = 'From: ' . $correo . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+if(isset(_$POST['enviar'])){
+	if(!empty($_POST['nombre'])  && !empty($_POST['correo']) && !empty($_POST['fono']) && !empty($_POST['mensaje'])){
 
-$mensaje = "Nombre Contacto: " . $nombre . ",\r\n";
-$mensaje .= "E-Mail Contacto: " . $correo . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+		$nombre = $_POST['nombre'];
+		$correo = $_POST['correo'];
+		$fono = $_POST['fono'];
+		$mensaje = $_POST['mensaje'];
 
-$para = 'rorro.mix@gmail.com';
-$asunto = 'Contacto Web';
+		$header = 'From: ' . $correo . " \r\n";
+		$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+		$header .= "Mime-Version: 1.0 \r\n";
+		$header .= "Content-Type: text/plain";
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+		$mensaje = "Nombre Contacto: " . $nombre . ",\r\n";
+		$mensaje .= "E-Mail Contacto: " . $correo . " \r\n";
+		$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+		$mensaje .= "Enviado el " . date('d/m/Y', time());
 
-header("Location:index.html");
+		$para = 'rorro.mix@gmail.com';
+		$asunto = 'Contacto Web';
+
+		$mail = @mail($correo, $asunto, utf8_decode($mensaje), $header);
+		if($mail){
+			echo "<h4>Mensaje Enviado!</h4>";
+		}
+
+
+	}
+}
+
 ?>
